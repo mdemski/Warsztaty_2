aa8c8d32851c7e4f626d04164e2be49797c5122d
Marcin Demski <m.demski@outlook.com>
2019-05-03 13:08:40 +0200

    Created 4 string variables with information to connect to database. Added getConnection method to prepare connection to specified database (program_school).

---
+++ zadanie_1/ConnectionGen.java
@@ -0,0 +1,21 @@
+package zadanie_1;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionGen {
+    private static final String DB_NAME = "program_school";
+    private static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/" + DB_NAME + "?useSSL=false&characterEncoding=utf8&serverTimezone=UTC";
+    private static final String DB_USER_NAME = "root";
+    private static final String DB_USER_PASSWORD = "coderslab";
+
+    public static Connection getConnection() throws SQLException {
+        {
+            return DriverManager.getConnection(CONNECTION_STRING, DB_USER_NAME, DB_USER_PASSWORD);
+        }
+    }
+
+
+}
