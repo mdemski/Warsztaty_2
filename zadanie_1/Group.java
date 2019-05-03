f5118bf6e96caecf33142979d418a669bac51bf3
Marcin Demski <m.demski@outlook.com>
2019-05-03 13:18:57 +0200

    Created Group class. Added private variables that describe group object. Added getters and setters to this variables. Added 2 constructors.

---
+++ zadanie_1/Group.java
@@ -0,0 +1,31 @@
+package zadanie_1;
+
+public class Group {
+    private int id;
+    private String name;
+
+    Group() {
+
+    }
+
+    public Group(String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
